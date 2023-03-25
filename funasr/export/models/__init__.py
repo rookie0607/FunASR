2e9e5322efe6a363c6fd3841c88069f72e88125a
九耳 <mengzhe.cmz@alibaba-inc.com>
2023-03-25 14:54:59 +0800

    update conversion

--- funasr/export/models/__init__.py
+++ funasr/export/models/__init__.py
@@ -1,6 +1,10 @@
 from funasr.models.e2e_asr_paraformer import Paraformer, BiCifParaformer
+from funasr.punctuation.target_delay_transformer import TargetDelayTransformer
+from funasr.punctuation.vad_realtime_transformer import VadRealtimeTransformer
 from funasr.export.models.e2e_asr_paraformer import Paraformer as Paraformer_export
 from funasr.export.models.e2e_asr_paraformer import BiCifParaformer as BiCifParaformer_export
+from funasr.export.models.target_delay_transformer import TargetDelayTransformer as TargetDelayTransformer_export
+from funasr.export.models.vad_realtime_transformer import VadRealtimeTransformer as VadRealtimeTransformer_export
 from funasr.models.e2e_uni_asr import UniASR
 
 
@@ -9,5 +13,9 @@ def get_model(model, export_config=None):
         return BiCifParaformer_export(model, **export_config)
     elif isinstance(model, Paraformer):
         return Paraformer_export(model, **export_config)
+    elif isinstance(model, TargetDelayTransformer):
+        return TargetDelayTransformer_export(model, **export_config)
+    elif isinstance(model, VadRealtimeTransformer):
+        return VadRealtimeTransformer_export(model, **export_config)
     else:
         raise "Funasr does not support the given model type currently."
