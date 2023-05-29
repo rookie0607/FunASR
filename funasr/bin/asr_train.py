1468d8138b3e3970bf016e671b8af3f96e7a1222
kk <huangshangkun@stu.xju.edu.cn>
2023-05-29 17:10:44 +0800

    Revert "update m2met2 (#543)"
    This reverts commit 72b2f1a02331328aba9d4a2442ff9e36b7c7dc0a.

--- funasr/bin/asr_train.py
+++ funasr/bin/asr_train.py
@@ -46,8 +46,7 @@ if __name__ == '__main__':
     args = parse_args()
 
     # setup local gpu_id
-    if args.ngpu > 0:
-        os.environ['CUDA_VISIBLE_DEVICES'] = str(args.gpu_id)
+    os.environ['CUDA_VISIBLE_DEVICES'] = str(args.gpu_id)
 
     # DDP settings
     if args.ngpu > 1:
@@ -58,9 +57,9 @@ if __name__ == '__main__':
 
     # re-compute batch size: when dataset type is small
     if args.dataset_type == "small":
-        if args.batch_size is not None and args.ngpu > 0:
+        if args.batch_size is not None:
             args.batch_size = args.batch_size * args.ngpu
-        if args.batch_bins is not None and args.ngpu > 0:
+        if args.batch_bins is not None:
             args.batch_bins = args.batch_bins * args.ngpu
 
     main(args=args)
